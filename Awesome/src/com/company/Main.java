823daff239f2ce7c6b3c1d9e06ff689ff307b30e
Victor Bonchevski <victor.bonchevski@gmail.com>
2020-01-12 14:46:50 +0200

    for loop example done

--- Awesome/src/com/company/Main.java
+++ Awesome/src/com/company/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         // For loop
-
+        for (int i=0;i<=100;i+=10){
+            System.out.println(i);
+        }
     }
 }
