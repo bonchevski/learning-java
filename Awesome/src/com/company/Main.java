44fbd27ca8383d095e7ba6c5466031c5683adde9
Victor Bonchevski <victor.bonchevski@gmail.com>
2020-01-12 14:16:33 +0200

    basic operators

--- Awesome/src/com/company/Main.java
+++ Awesome/src/com/company/Main.java
@@ -3,8 +3,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args)   {
+        int num1 = 5;
+        int num2 = 3;
+    // Operators start
+        System.out.println(num1+num2);
+        System.out.println(num1*num2);
+        System.out.println(num1%num2);
+        System.out.println(num1^num2);
+        num1++;
+        System.out.println(num1+"Incremented");
+        num2--;
+        System.out.println(num2+"Decremented");
 
-    // Operators start 
+
 
     }
 }
