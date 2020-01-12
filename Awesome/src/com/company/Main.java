fba3e98085948b0d3e5014551918a294e2eeeb57
Victor Bonchevski <victor.bonchevski@gmail.com>
2020-01-12 14:37:00 +0200

    Switch statement lecture start

--- Awesome/src/com/company/Main.java
+++ Awesome/src/com/company/Main.java
@@ -3,19 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args)   {
-     // If else statements
-        int i = 7;
-        if (i>4 && i<10){
-            System.out.println("This is true");
-        }
-        else if(i==25){
-            System.out.println("Not as good , but the value is 5");
-        }
-        else if(i==14){
-            System.out.println("Not as good , but the value is 5");
-        }
-        else{
-            System.out.println("Fallback line none of the conditions were true");
-        }
+     // Switch statement lecture
+        
     }
 }
