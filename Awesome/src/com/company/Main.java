735b8e25d4eaca206485bb3e8390b58d928e47a4
Victor Bonchevski <victor.bonchevski@gmail.com>
2020-01-12 14:47:10 +0200

    While loop start

--- Awesome/src/com/company/Main.java
+++ Awesome/src/com/company/Main.java
@@ -3,9 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        // For loop
-        for (int i=0;i<=100;i+=10){
-            System.out.println(i);
-        }
+        // While loop
+
     }
 }
