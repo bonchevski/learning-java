82af7dbc979f2263f1bcb7eda16ed53fed1e442a
Victor Bonchevski <victor.bonchevski@gmail.com>
2020-01-12 14:11:45 +0200

    end of Reading Input

--- Awesome/src/com/company/Main.java
+++ Awesome/src/com/company/Main.java
@@ -1,11 +1,19 @@
 package com.company;
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
     // Will be using the java.io.BufferedReader; for console input lecture
+    //import java.io.InputStreamReader; lets us read the input and the exception is for errors
 
-        
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String name = reader.readLine();
+
+        System.out.println(name);
+
 
 
     }
