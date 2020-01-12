9e9908b789ac8ca17f6691ab7971d313ee531bf6
Victor Bonchevski <victor.bonchevski@gmail.com>
2020-01-12 14:57:18 +0200

    while loop done , dowhile next

--- Awesome/src/com/company/Main.java
+++ Awesome/src/com/company/Main.java
@@ -3,7 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        // While loop
-
+        // For loop
+        System.out.println("For Loop");
+        for (int i=0;i<=100;i+=10){
+            System.out.println(i);
+        }
+        // While Loop
+        System.out.println("While loop");
+        int j = 0;
+        while (j<100){
+            j++;
+            System.out.println(j);
+        }
     }
 }
