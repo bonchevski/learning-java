d7727c246e3cc96ff3aeef88516d1b66b98a0e6a
Victor Bonchevski <victor.bonchevski@gmail.com>
2020-01-12 14:43:47 +0200

    For Loop beginning

--- Awesome/src/com/company/Main.java
+++ Awesome/src/com/company/Main.java
@@ -3,26 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        // Switch statements
-        int var = 33;
-        switch (var) {
-            case 0:
-                System.out.println("number 0");
-                break;
-            case 1:
-                System.out.println("number 1");
-                break;
-            case 2:
-                System.out.println("number 2");
-                break;
-            case 3:
-                System.out.println("number 3");
-                break;
-            case 4:
-                System.out.println("number 4");
-                break;
-            default:
-                System.out.println("The number was not valid");
-        }
+        // For loop
+
     }
 }
