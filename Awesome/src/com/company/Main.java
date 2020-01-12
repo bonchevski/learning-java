69230d87c1e2d42b3d9bf67013f952edf8aac09c
Victor Bonchevski <victor.bonchevski@gmail.com>
2020-01-12 13:48:48 +0200

    first lecture

---
+++ Awesome/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println("Print the line");
+
+        int age;
+        int apartment;
+        apartment= 9;
+        age = 24;
+        System.out.println(age);
+        System.out.println(apartment);
+
+
+    }
+}
