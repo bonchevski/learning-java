eef499e2e2793a2b5d8a806d2a905831e8c7b5a2
Victor Bonchevski <victor.bonchevski@gmail.com>
2020-01-12 14:07:27 +0200

    end of constants lecture

--- Awesome/src/com/company/Main.java
+++ Awesome/src/com/company/Main.java
@@ -3,12 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-    // final means that the float pi is going to be the constant value hence it cannot be assigned below
     final float pi = 3.14159f;
         System.out.println(pi);
 
-    pi=4;
-        System.out.println(pi);
 
     }
 }
